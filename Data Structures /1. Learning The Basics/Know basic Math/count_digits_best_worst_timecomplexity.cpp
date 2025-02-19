edfcf4ee1490189e37f6b2a08310428e00b3d62b
Gelli T N S V Vinay Raja Mohan <mail2vinay2004@gmail.com>
2025-02-19 19:26:15 +0530

    Create count_digits_best_worst_timecomplexity.cpp

---
+++ Data Structures /1. Learning The Basics/Know basic Math/count_digits_best_worst_timecomplexity.cpp
@@ -0,0 +1,15 @@
+int evenlyDivides(int n) {
+            int count = 0;
+    int temp = n;  // Use a temporary variable to hold the original value of n
+
+    while (temp > 0) {
+        int digit = temp % 10;  // Get the last digit
+        // Check if the digit is not zero and divides n evenly
+        if (digit != 0 && n % digit == 0) {
+            count++;
+        }
+        temp /= 10;  // Remove the last digit
+    }
+
+    return count; //O(log10 (n))
+}
