78600a12cf10e6c9d196e6ea5573bf9e88a583b5
Vinay-raja-mohan <mail2vinay2004@gmail.com>
2025-05-30 15:44:29 +0530

    first commit

---
+++ Technologies/Data Structures/1. Learning The Basics/Things to Know in C++/Fibonacci_worst_time.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+        int n;
+        int a = 1, b = 1;
+    int result;
+        cin >> n;
+        if(n==1 || n==2){
+                cout << 1;
+        }
+        else{
+                for (int i = 3; i <= n; ++i) {
+                        result = a + b; 
+                        a = b; 
+                        b = result;
+    }
+    cout <<  result; 
+        }  
+}
---
+++ Technologies/Data Structures/1. Learning The Basics/Things to Know in C++/reversing_Nnumbers_BestAndWorstTimeComplexity.cpp
@@ -0,0 +1,15 @@
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Write C++ code here
+int n;
+cin >> n;
+while(n>0){
+    cout << n << " ";
+    n--;
+}
+    
+    return 0;
+}
